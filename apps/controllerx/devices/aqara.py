0f9899a2cca25d843625fc468c1d7b1c30a1fef5
crserran <criserrano93@gmail.com>
2020-02-18 01:41:20 +0100

    [MFKZQ01LM] Xiaomi Aqara Smart Cube zigbee2mqtt integration for a single light control

--- apps/controllerx/devices/aqara.py
+++ apps/controllerx/devices/aqara.py
@@ -26,3 +26,29 @@ class DoubleKeyWirelessAqaraController(LightController):
             "right_double": Light.CLICK_BRIGHTNESS_UP,
             "right_long": Light.CLICK_BRIGHTNESS_DOWN,
         }
+
+
+class MFKZQ01LMController(LightController):
+    """
+    This controller allows movement actions for Xiaomi Aqara Smart Cube as
+    shake, wakeup, fall, slide, flip180 or 90 and rotate_left or right. 
+    Then from the apps.yaml the needed actions can be included and create
+    different instances for different lights.
+    """
+
+    # Different states reported from the controller:
+    # shake, wakeup, fall, tap, slide, flip180
+    # flip90, rotate_left and rotate_right
+
+    def get_z2m_actions_mapping(self):
+        return {
+            "shake": Light.TOGGLE,
+            "wakeup": Light.RELEASE,
+            "fall": Light.RELEASE,
+            "tap": Light.HOLD_COLOR_UP,
+            "slide": Light.ON_FULL_BRIGHTNESS,
+            "flip180": Light.CLICK_COLOR_UP,
+            "flip90": Light.CLICK_COLOR_DOWN,
+            "rotate_left": Light.CLICK_BRIGHTNESS_DOWN,
+            "rotate_right": Light.CLICK_BRIGHTNESS_UP,
+        }
