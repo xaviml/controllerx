0d55c436ab1ac7113cf85a8a64dd0e4eb336ccf3
sreknob <sreknob@hotmail.com>
2020-02-21 13:43:58 -0500

    update for proper states, release comment

--- apps/controllerx/devices/samjin.py
+++ apps/controllerx/devices/samjin.py
@@ -3,15 +3,16 @@ from const import Light
 
 class SamjinButton(LightController):
     """
-    This controller allows press, double press, and hold.
+    This controller allows click, double click, and hold.
+    No release command is sent.
     """
 
     # Different states reported from the controller:
-    # press, double press, hold
+    # button_single, button_double, button_hold
 
     def get_zha_actions_mapping(self):
         return {
-            "press": Light.TOGGLE,
-            "double_press": Light.ON_FULL_BRIGHTNESS,
-            "long_press": Light.HOLD_BRIGHTNESS_TOGGLE,
+            "button_single": Light.TOGGLE,
+            "button_double": Light.ON_FULL_BRIGHTNESS,
+            "button_hold": Light.SET_HALF_BRIGHTNESS,
         }
