db806bcef42a611114bd9141d6febef906c3af11
crserran <criserrano93@gmail.com>
2020-02-24 20:50:13 +0100

    Support DeConz for specific event handling types

--- apps/controllerx/core/integration/__init__.py
+++ apps/controllerx/core/integration/__init__.py
@@ -10,19 +10,20 @@ def _import_modules(file_dir, package):
         importlib.import_module("." + name, package)
 
 
-def get_integrations(controller):
+def get_integrations(controller, kwargs):
     _import_modules(__file__, __package__)
     subclasses = [c for c in Integration.__subclasses__()]
     integrations = [
-        cls_(controller) for cls_ in subclasses if len(cls_.__subclasses__()) == 0
+        cls_(controller, kwargs) for cls_ in subclasses if len(cls_.__subclasses__()) == 0
     ]
     return integrations
 
 
 class Integration(abc.ABC):
-    def __init__(self, name, controller):
+    def __init__(self, name, controller, kwargs):
         self.name = name
         self.controller = controller
+        self.kwargs = kwargs
 
     @abc.abstractmethod
     def get_actions_mapping(self, controller_id):
@@ -31,3 +32,4 @@ class Integration(abc.ABC):
     @abc.abstractmethod
     def listen_changes(self):
         pass
+        
