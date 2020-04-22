0dc6fa90637b8390d50df7b93736959670989dde
Gabriel Augendre <gabriel@augendre.info>
2020-04-22 21:15:08 +0200

    Write some tests

---
+++ tests/core/type/switch_controller_test.py
@@ -0,0 +1,44 @@
+import pytest
+
+from core.controller import TypeController
+from tests.utils import hass_mock
+from core.type.switch_controller import SwitchController
+
+
+@pytest.fixture
+@pytest.mark.asyncio
+async def sut(hass_mock, mocker):
+    c = SwitchController()
+    mocker.patch.object(TypeController, "initialize")
+    c.args = {"switch": "switch.test"}
+    await c.initialize()
+    return c
+
+
+@pytest.mark.asyncio
+async def test_initialize(sut):
+    await sut.initialize()
+    assert sut.switch == "switch.test"
+
+
+@pytest.mark.asyncio
+async def test_turn_on(sut, mocker):
+    called_service_patch = mocker.patch.object(sut, "call_service")
+    await sut.on()
+    called_service_patch.assert_called_once_with("switch/turn_on", entity_id=sut.switch)
+
+
+@pytest.mark.asyncio
+async def test_turn_off(sut, mocker):
+    called_service_patch = mocker.patch.object(sut, "call_service")
+    await sut.off()
+    called_service_patch.assert_called_once_with(
+        "switch/turn_off", entity_id=sut.switch
+    )
+
+
+@pytest.mark.asyncio
+async def test_toggle(sut, mocker):
+    called_service_patch = mocker.patch.object(sut, "call_service")
+    await sut.toggle()
+    called_service_patch.assert_called_once_with("switch/toggle", entity_id=sut.switch)
