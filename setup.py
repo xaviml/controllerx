d5d37e173b635e3fddafa8040c0433eea6d10840
Xavi <xaviml.93@gmail.com>
2020-04-24 23:54:43 +0200

    test(fix): fixes tests for python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
+    name="controllerx",
+    python_requires=">=3.6",
     package_dir={"": "apps/controllerx"},
     packages=find_packages(where="apps/controllerx"),
     py_modules=["utils", "controllerx", "version", "const"],
