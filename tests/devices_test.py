9ac8a617618ff0a68a6faa065517dc843dc145fd
Xavi <xaviml.93@gmail.com>
2020-04-29 19:48:13 +0200

    test(device): add `default_delay` to check is a positive value

--- tests/devices_test.py
+++ tests/devices_test.py
@@ -1,18 +1,27 @@
+from core.controller import ReleaseHoldController
 from tests.test_utils import hass_mock, get_instances
 import devices as devices_module
 from core import Controller
 
 
-def check_mapping(mapping, all_possible_actions, device_name):
+def check_mapping(mapping, all_possible_actions, device):
+    device_name = device.__class__.__name__
     if mapping is None:
         return
+    if issubclass(device.__class__, ReleaseHoldController):
+        delay = device.default_delay()
+        if delay < 0:
+            raise ValueError(
+                f"`default_delay` should be a positive integer and the value is `{delay}`. "
+                f"Device class: {device_name}"
+            )
     for k, v in mapping.items():
-        if type(v) != str:
+        if not isinstance(v, str):
             raise ValueError(
                 "The value from the mapping should be a string, matching "
-                + "one of the actions from the controller. "
-                + f"The possible actions are: {all_possible_actions}. "
-                + f"Device class: {device_name}"
+                "one of the actions from the controller. "
+                f"The possible actions are: {all_possible_actions}. "
+                f"Device class: {device_name}"
             )
 
         if v not in all_possible_actions:
@@ -27,14 +36,13 @@ def test_devices(hass_mock):
         devices_module.__file__, devices_module.__package__, Controller
     )
     for device in devices:
-        device_name = device.__class__.__name__
         type_actions_mapping = device.get_type_actions_mapping()
         if type_actions_mapping is None:
             continue
         possible_actions = list(type_actions_mapping.keys())
         mappings = device.get_z2m_actions_mapping()
-        check_mapping(mappings, possible_actions, device_name)
+        check_mapping(mappings, possible_actions, device)
         mappings = device.get_deconz_actions_mapping()
-        check_mapping(mappings, possible_actions, device_name)
+        check_mapping(mappings, possible_actions, device)
         mappings = device.get_zha_actions_mapping()
-        check_mapping(mappings, possible_actions, device_name)
+        check_mapping(mappings, possible_actions, device)
