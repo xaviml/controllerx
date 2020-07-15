9cac06b983c36d6def87c3ac418221c22e576707
Xavi <xaviml.93@gmail.com>
2020-07-15 18:43:22 +0200

    feat(device): add specific ZHA parser for Legrand Simple and Double Wall Controller
    related to #98

--- apps/controllerx/cx_devices/legrand.py
+++ apps/controllerx/cx_devices/legrand.py
@@ -1,16 +1,49 @@
+from typing import Optional
+
 from cx_const import Light, TypeActionsMapping
 from cx_core import LightController
 
 
-class SimpleWallSwitchController(LightController):
-    # Different states reported from the controller:
-    # on, off, brightness_up, brightness_down, brightness_stop
+def get_zha_action_LegrandWallController(data: dict) -> Optional[str]:
+    endpoint_id = data.get("endpoint_id", 1)
+    command = action = data["command"]
+    args = data.get("args", {})
+    args_mapping = {0: "up", 1: "down"}
+    if command == "move":
+        action = "_".join((action, args_mapping[args[0]]))
+    action = "_".join((str(endpoint_id), action))
+    return action
 
+
+class LegrandSimpleWallLightController(LightController):
     def get_zha_actions_mapping(self) -> TypeActionsMapping:
         return {
-            "on": Light.ON,
-            "off": Light.OFF,
-            "move_0_255": Light.HOLD_BRIGHTNESS_UP,
-            "move_1_255": Light.HOLD_BRIGHTNESS_DOWN,
-            "stop": Light.RELEASE,
+            "1_on": Light.ON,
+            "1_off": Light.OFF,
+            "1_move_up": Light.HOLD_BRIGHTNESS_UP,
+            "1_move_down": Light.HOLD_BRIGHTNESS_DOWN,
+            "1_stop": Light.RELEASE,
         }
+
+    def get_zha_action(self, data: dict) -> Optional[str]:
+        return get_zha_action_LegrandWallController(data)
+
+
+class LegrandDoubleWallLightController(LightController):
+    # TODO: define the mapping for this controller
+    def get_zha_actions_mapping(self) -> TypeActionsMapping:
+        return {
+            "1_on": Light.ON,
+            "1_off": Light.OFF,
+            "1_move_up": Light.HOLD_BRIGHTNESS_UP,
+            "1_move_down": Light.HOLD_BRIGHTNESS_DOWN,
+            "1_stop": Light.RELEASE,
+            "2_on": Light.ON,
+            "2_off": Light.OFF,
+            "2_move_up": Light.HOLD_BRIGHTNESS_UP,
+            "2_move_down": Light.HOLD_BRIGHTNESS_DOWN,
+            "2_stop": Light.RELEASE,
+        }
+
+    def get_zha_action(self, data: dict) -> Optional[str]:
+        return get_zha_action_LegrandWallController(data)
