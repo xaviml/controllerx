450adcc909cecca69057f28db2a243f32f62f2e7
Xavi <xaviml.93@gmail.com>
2020-09-12 10:23:23 +0200

    feat(device): add deconz support for WXCJKG13LMLightController
    related to #117

--- apps/controllerx/cx_devices/aqara.py
+++ apps/controllerx/cx_devices/aqara.py
@@ -199,26 +199,66 @@ class WXCJKG13LMLightController(LightController):
         return {
             "button_1_single": Light.OFF,
             "button_1_double": Light.SYNC,
+            # "button_1_triple": "", # Nothing
             # "button_1_hold": "", # Nothing
             # "button_1_release": "", # Nothing
             "button_2_single": Light.ON,
             "button_2_double": Light.SYNC,
+            # "button_2_triple": "", # Nothing
             # "button_2_hold": "", # Nothing
             # "button_2_release": "", # Nothing
             "button_3_single": Light.CLICK_BRIGHTNESS_DOWN,
             "button_3_double": Light.ON_MIN_BRIGHTNESS,
+            # "button_3_triple": "", # Nothing
             "button_3_hold": Light.HOLD_BRIGHTNESS_DOWN,
             "button_3_release": Light.RELEASE,
             "button_4_single": Light.CLICK_BRIGHTNESS_UP,
             "button_4_double": Light.ON_FULL_BRIGHTNESS,
+            # "button_4_triple": "", # Nothing
             "button_4_hold": Light.HOLD_BRIGHTNESS_UP,
             "button_4_release": Light.RELEASE,
             "button_5_single": Light.CLICK_COLOR_DOWN,
             "button_5_double": Light.ON_MIN_COLOR_TEMP,
+            # "button_5_triple": "", # Nothing
             "button_5_hold": Light.HOLD_COLOR_DOWN,
             "button_5_release": Light.RELEASE,
             "button_6_single": Light.CLICK_COLOR_UP,
             "button_6_double": Light.ON_FULL_COLOR_TEMP,
+            # "button_6_triple": "", # Nothing
             "button_6_hold": Light.HOLD_COLOR_UP,
             "button_6_release": Light.RELEASE,
+        }
+
+    def get_deconz_actions_mapping(self) -> TypeActionsMapping:
+        return {
+            1002: Light.OFF,
+            1004: Light.SYNC,
+            # 1005: "",  # Nothing
+            # 1001: "",  # Nothing
+            # 1003: "",  # Nothing
+            2002: Light.ON,
+            2004: Light.SYNC,
+            # 2005: "",  # Nothing
+            # 2001: "",  # Nothing
+            # 2003: "",  # Nothing
+            3002: Light.CLICK_BRIGHTNESS_DOWN,
+            3004: Light.ON_MIN_BRIGHTNESS,
+            # 3005: "",  # Nothing
+            3001: Light.HOLD_BRIGHTNESS_DOWN,
+            3003: Light.RELEASE,
+            4002: Light.CLICK_BRIGHTNESS_UP,
+            4004: Light.ON_FULL_BRIGHTNESS,
+            # 4005: "",  # Nothing
+            4001: Light.HOLD_BRIGHTNESS_UP,
+            4003: Light.RELEASE,
+            5002: Light.CLICK_COLOR_DOWN,
+            5004: Light.ON_MIN_COLOR_TEMP,
+            # 5005: "",  # Nothing
+            5001: Light.HOLD_COLOR_DOWN,
+            5003: Light.RELEASE,
+            6002: Light.CLICK_COLOR_UP,
+            6004: Light.ON_FULL_COLOR_TEMP,
+            # 6005: "",  # Nothing
+            6001: Light.HOLD_COLOR_UP,
+            6003: Light.RELEASE,
         }
