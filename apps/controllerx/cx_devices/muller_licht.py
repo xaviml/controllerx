dacad8fdd93e31ca949fc7c6f2a0247b73dd0d4d
Xavier Moreno <xaviml.93@gmail.com>
2020-09-19 23:53:03 +0200

    feat(device): MLI-404011
    Add deCONZ support for new controller: MLI-404011 (Müller Licht)
    
    related to #111

---
+++ apps/controllerx/cx_devices/muller_licht.py
@@ -0,0 +1,24 @@
+from cx_const import Light, TypeActionsMapping
+from cx_core import LightController
+
+
+class MLI404011LightController(LightController):
+    def get_deconz_actions_mapping(self) -> TypeActionsMapping:
+        return {
+            1002: Light.TOGGLE,
+            2001: Light.HOLD_BRIGHTNESS_UP,
+            2002: Light.CLICK_BRIGHTNESS_UP,
+            2003: Light.RELEASE,
+            3001: Light.HOLD_BRIGHTNESS_DOWN,
+            3002: Light.CLICK_BRIGHTNESS_DOWN,
+            3003: Light.RELEASE,
+            4002: Light.CLICK_COLOR_UP,
+            5002: Light.CLICK_COLOR_DOWN,
+            # 6002: "",  # Color ring press
+            # 7002: "",  # reading button
+            # 8002: "",  # sunset button
+            # 9002: "",  # party button
+            # 10002: "",  # night button
+            # 11002: "",  # fire button
+            # 12002: "",  # heart button
+        }
