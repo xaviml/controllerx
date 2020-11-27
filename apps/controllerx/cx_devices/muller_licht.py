d0522ef095f85e859cedff3b8307b39810e6f3d0
Xavier Moreno <xaviml.93@gmail.com>
2020-11-27 22:29:30 +0100

    feat(device): add Muller-Licht controller support for z2m
    related to #176

--- apps/controllerx/cx_devices/muller_licht.py
+++ apps/controllerx/cx_devices/muller_licht.py
@@ -3,6 +3,26 @@ from cx_core import LightController
 
 
 class MLI404011LightController(LightController):
+    def get_z2m_actions_mapping(self) -> TypeActionsMapping:
+        return {
+            "on": Light.TOGGLE,
+            "off": Light.TOGGLE,
+            "brightness_down_click": Light.CLICK_BRIGHTNESS_DOWN,
+            "brightness_down_hold": Light.HOLD_BRIGHTNESS_DOWN,
+            "brightness_down_release": Light.RELEASE,
+            "brightness_up_click": Light.CLICK_BRIGHTNESS_UP,
+            "brightness_up_hold": Light.HOLD_BRIGHTNESS_DOWN,
+            "brightness_up_release": Light.RELEASE,
+            # color_temp: "" # warm or cold
+            # color_wheel: "" # Color ring press
+            # "scene_3": "",  # reading button
+            # "scene_1": "",  # sunset button
+            # "scene_2": "",  # party button
+            # "scene_6": "",  # night button
+            # "scene_4": "",  # fire button
+            # "scene_5": "",  # heart button
+        }
+
     def get_deconz_actions_mapping(self) -> TypeActionsMapping:
         return {
             1002: Light.TOGGLE,
