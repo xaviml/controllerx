5f87fedf17e13e6167e7efc4a442dbe859d08f1f
Xavier Moreno <xaviml.93@gmail.com>
2020-12-18 17:26:27 +0100

    fix(device): change terncy mapping for 5 clicks

--- apps/controllerx/cx_devices/terncy.py
+++ apps/controllerx/cx_devices/terncy.py
@@ -24,7 +24,7 @@ class TerncySD01LightController(LightController):
             "button_double": Light.ON_FULL_BRIGHTNESS,
             "button_triple": Light.ON_MIN_BRIGHTNESS,
             "button_quadruple": Light.SET_HALF_BRIGHTNESS,
-            "button_quintuple": Light.SET_HALF_BRIGHTNESS,
+            "button_quintuple": Light.SET_HALF_COLOR_TEMP,
         }
 
     def get_zha_action(self, data: EventData) -> str:
