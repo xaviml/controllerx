b4212609df4962167ad5278725ca1c74b89ae4ba
Xavier Moreno <xaviml.93@gmail.com>
2021-01-23 18:23:43 +0100

    feat(device): add support for ZB-5122 RGB genie
    related to #228

--- apps/controllerx/cx_devices/rgb_genie.py
+++ apps/controllerx/cx_devices/rgb_genie.py
@@ -1,5 +1,8 @@
-from cx_const import DefaultActionsMapping, Light
+from cx_const import DefaultActionsMapping, Light, PredefinedActionsMapping
 from cx_core import LightController
+from cx_core.controller import action
+from cx_core.integration import EventData
+from cx_core.integration.zha import ZHAIntegration
 
 
 class ZB5121LightController(LightController):
@@ -14,3 +17,47 @@ class ZB5121LightController(LightController):
             # "recall_0_1": "",  # Click clapperboard
             "stop": Light.RELEASE,  # long release
         }
+
+
+class ZB5122LightController(LightController):
+
+    MOVE_TO_COLOR = "move_to_color"
+
+    @action
+    async def move_to_color(self, extra: EventData) -> None:
+        if isinstance(self.integration, ZHAIntegration):
+            await self.on(rgb_color=extra["args"])
+
+    def get_predefined_actions_mapping(self) -> PredefinedActionsMapping:
+        parent_mapping = super().get_predefined_actions_mapping()
+        mapping: PredefinedActionsMapping = {
+            ZB5122LightController.MOVE_TO_COLOR: self.move_to_color,
+        }
+        parent_mapping.update(mapping)
+        return parent_mapping
+
+    def get_zha_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "on": Light.ON,  # Click light on
+            "off": Light.OFF,  # Click light off
+            "hold_brightness_up": Light.HOLD_BRIGHTNESS_UP,  # Hold light on
+            "hold_brightness_down": Light.HOLD_BRIGHTNESS_DOWN,  # Hold light off
+            "stop": Light.RELEASE,  # long release
+            "move_to_color": ZB5122LightController.MOVE_TO_COLOR,  # click RGB
+            "move_hue": Light.HOLD_XY_COLOR_UP,  # hold RGB
+            "stop_move_hue": Light.RELEASE,  # release RGB
+            "move_to_color_temp": Light.ON_FULL_COLOR_TEMP,  # click CW
+            "move_color_temp": Light.HOLD_COLOR_TEMP_TOGGLE,  # hold CW
+            "stop_move_step": Light.RELEASE,  # release CW
+            # "recall_0_1": "",  # Click clapperboard
+        }
+
+    def get_zha_action(self, data: EventData) -> str:
+        command: str = data["command"]
+        if command == "move_with_on_off":
+            return (
+                "hold_brightness_up" if data["args"][0] == 0 else "hold_brightness_down"
+            )
+        elif command == "move_hue":
+            return "stop_move_hue" if tuple(data["args"]) == (0, 0) else "move_hue"
+        return command
---
+++ tests/unit_tests/cx_devices/rgb_genie_test.py
@@ -0,0 +1,22 @@
+import pytest
+from cx_core.integration import EventData
+from cx_devices.rgb_genie import ZB5122LightController
+
+
+@pytest.mark.parametrize(
+    "data, expected_action",
+    [
+        ({"command": "on"}, "on"),
+        ({"command": "off"}, "off"),
+        ({"command": "stop"}, "stop"),
+        ({"command": "move_to_color"}, "move_to_color"),
+        ({"command": "move_with_on_off", "args": [0, 50]}, "hold_brightness_up"),
+        ({"command": "move_with_on_off", "args": [1, 50]}, "hold_brightness_down"),
+        ({"command": "move_hue", "args": [0, 0]}, "stop_move_hue"),
+        ({"command": "move_hue", "args": [1, 2]}, "move_hue"),
+    ],
+)
+def test_zha_action_MFKZQ01LMLightController(data: EventData, expected_action: str):
+    sut = ZB5122LightController()  # type: ignore
+    action = sut.get_zha_action(data)
+    assert action == expected_action
