fefc989e0c62eba9a87d2b1605117c557c2f1512
Xavier Moreno <xaviml.93@gmail.com>
2021-10-28 22:27:24 +0200

    feat(device): add media player controller for TerncySD01
    related to #377

--- apps/controllerx/cx_devices/terncy.py
+++ apps/controllerx/cx_devices/terncy.py
@@ -1,5 +1,5 @@
-from cx_const import DefaultActionsMapping, Light
-from cx_core import LightController
+from cx_const import DefaultActionsMapping, Light, MediaPlayer
+from cx_core import LightController, MediaPlayerController
 from cx_core.integration import EventData
 
 
@@ -25,6 +25,24 @@ class TerncySD01LightController(LightController):
             "button_triple": Light.ON_MIN_BRIGHTNESS,
             "button_quadruple": Light.SET_HALF_BRIGHTNESS,
             "button_quintuple": Light.SET_HALF_COLOR_TEMP,
+            "rotate_left": Light.CLICK_BRIGHTNESS_DOWN,
+            "rotate_right": Light.CLICK_BRIGHTNESS_UP,
+        }
+
+    def get_zha_action(self, data: EventData) -> str:
+        return data["command"]
+
+
+class TerncySD01MediaPlayerController(MediaPlayerController):
+    def get_zha_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "button_single": MediaPlayer.PLAY_PAUSE,
+            "button_double": MediaPlayer.MUTE,
+            "button_triple": MediaPlayer.NEXT_TRACK,
+            "button_quadruple": MediaPlayer.PREVIOUS_TRACK,
+            "button_quintuple": MediaPlayer.NEXT_SOURCE,
+            "rotate_left": MediaPlayer.CLICK_VOLUME_DOWN,
+            "rotate_right": MediaPlayer.CLICK_VOLUME_UP,
         }
 
     def get_zha_action(self, data: EventData) -> str:
