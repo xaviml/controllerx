b5a504de64d041a1c33bfdfb1cb077be3085910d
Xavier Moreno <xaviml.93@gmail.com>
2022-04-13 12:12:05 +0200

    fix(homematic): controller filter is done on event callback method
    related to #421

--- apps/controllerx/cx_core/integration/homematic.py
+++ apps/controllerx/cx_core/integration/homematic.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Set
 
 from appdaemon.plugins.hass.hassapi import Hass
 from cx_const import DefaultActionsMapping
@@ -7,21 +7,22 @@ from cx_core.integration import EventData, Integration
 
 class HomematicIntegration(Integration):
     name = "homematic"
+    _registererd_controller_ids: Set[str] = set()
 
     def get_default_actions_mapping(self) -> Optional[DefaultActionsMapping]:
         return self.controller.get_homematic_actions_mapping()
 
     async def listen_changes(self, controller_id: str) -> None:
+        self._registererd_controller_ids.add(controller_id)
         await Hass.listen_event(
-            self.controller,
-            self.event_callback,
-            "homematic.keypress",
-            name=controller_id,
+            self.controller, self.event_callback, "homematic.keypress"
         )
 
     async def event_callback(
         self, event_name: str, data: EventData, kwargs: dict
     ) -> None:
+        if data["name"] not in self._registererd_controller_ids:
+            return
         param = data["param"]
         channel = data["channel"]
         action = f"{param}_{channel}"
