626bffa89db39e90f4eb54972b0e011822e86a45
Sabaat Ahmad <sabaata@amazon.com>
2022-04-15 07:15:42 -0700

    Added z2m support for SNZB-01

--- apps/controllerx/cx_devices/sonoff.py
+++ apps/controllerx/cx_devices/sonoff.py
@@ -4,6 +4,13 @@ from cx_core.integration import EventData
 
 
 class SNZB01LightController(LightController):
+    def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            "single": Light.TOGGLE,  # single click
+            "double": Light.ON_FULL_BRIGHTNESS,  # double click
+            "long": Light.ON_MIN_BRIGHTNESS,  # hold
+        }
+
     def get_zha_actions_mapping(self) -> DefaultActionsMapping:
         return {
             "toggle": Light.TOGGLE,  # single click
