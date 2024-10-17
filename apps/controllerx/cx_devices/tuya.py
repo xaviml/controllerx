57a75fe2af5a4f9f68c4b5697707b920b340ad4d
Strauman <someone@example.com>
2024-10-17 20:51:34 +0200

    Corrects type

--- apps/controllerx/cx_devices/tuya.py
+++ apps/controllerx/cx_devices/tuya.py
@@ -122,10 +122,11 @@ class TuYaERS10TZBVKAAMediaPlayerController(MediaPlayerController):
 class TS0042LightController(LightController):
     def get_zha_actions_mapping(self) -> DefaultActionsMapping:
         return {
-            "1_remote_button_short_press": [Light.ON, Light.CLICK_BRIGHTNESS_UP],
+            "1_remote_button_short_press": Light.ON,
+            "1_remote_button_double_press": Light.CLICK_BRIGHTNESS_UP,
             "1_remote_button_long_press": Light.ON_FULL_BRIGHTNESS,
-            "2_remote_button_short_press": Light.CLICK_BRIGHTNESS_DOWN,
-            "2_remote_button_double_press": Light.OFF
+            "2_remote_button_short_press": Light.OFF,
+            "2_remote_button_double_press": Light.ON_MIN_BRIGHTNESS
         }
 
     def get_zha_action(self, data: EventData) -> str:
